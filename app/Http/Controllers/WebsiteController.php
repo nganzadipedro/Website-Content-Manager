71d572cc45a3bdd85815bc4d525a3c407f8821c0
nganzadipedro <nganzadipedro.emp@gmail.com>
2025-08-22 15:04:05 +0100

    limpeza de classes desnecessarias

---
+++ app/Http/Controllers/WebsiteController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Denuncia;
+use App\Models\Galeria;
+use App\Models\Mensagem;
+use App\Models\Noticia;
+use Auth;
+use Illuminate\Http\Request;
+
+class WebsiteController extends Controller
+{
+    public function home(Request $request)
+    {
+
+    }
+
+    public function messages()
+    {
+
+    }
+
+    public function contact()
+    {
+
+    }
+
+    public function members()
+    {
+
+    }
+
+    public function services()
+    {
+
+    }
+
+    public function complaints()
+    {
+
+    }
+
+    public function judicial_assistance()
+    {
+
+    }
+
+}
